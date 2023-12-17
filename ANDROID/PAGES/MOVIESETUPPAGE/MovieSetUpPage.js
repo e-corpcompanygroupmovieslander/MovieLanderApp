0470193479ab63fe37359e08cfb6ba6c186c4a85
e-corpcompanygroupmovieslander <movielander1999@gmail.com>
2023-12-17 06:40:44 -0800

    ADDED THE MOVIE DETAILS HOLDER

--- ANDROID/PAGES/MOVIESETUPPAGE/MovieSetUpPage.js
+++ ANDROID/PAGES/MOVIESETUPPAGE/MovieSetUpPage.js
@@ -19,6 +19,10 @@ const MOVIESETUPPAGE=(DIV,ADD,CLEAR,DISPLAY,ICONS,ADVANCE,element)=>{
             <iframe class='Player' src="https://www.youtube-nocookie.com/embed/${element.MoveTrailer}?autoplay=1&mute=1&rel=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
         
         </div>
+
+        <button class='WatchMovieButton'>Play</button>
+
+        <div class='MovieDetailsHolder'></div>
     
 
         <div class='AndroidFooter'>
