c81ec220c9f827160472ddb37c00a4f6416a41ea
e-corpcompanygroupmovieslander <movielander1999@gmail.com>
2023-12-17 11:27:41 -0800

    Image Updates

--- APIS/IconsApi.js
+++ APIS/IconsApi.js
@@ -1,6 +1,6 @@
-const ICONSPATH=`../ASSETS/ICONS/`;
+//const ICONSPATH=`../ASSETS/ICONS/`;
 
-//const ICONSPATH=`https://e-corpcompanygroupmovieslander.github.io/MovieLanderApp/ASSETS/ICONS/`;
+const ICONSPATH=`https://e-corpcompanygroupmovieslander.github.io/MovieLanderApp/ASSETS/ICONS/`;
 
 
 export{ICONSPATH}
