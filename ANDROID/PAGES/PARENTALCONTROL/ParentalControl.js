abc3309f8cb1ae30544ac85b633022fa2816c078
e-corpcompanygroupmovieslander <movielander1999@gmail.com>
2023-12-18 07:48:18 +0300

    Parental Control Worked On

--- ANDROID/PAGES/PARENTALCONTROL/ParentalControl.js
+++ ANDROID/PAGES/PARENTALCONTROL/ParentalControl.js
@@ -2,6 +2,8 @@ import { ANDROIDSETTINGSPAGE } from "../SETTINGSPAGE/SettingsPage.js";
 
 const ANDROIDPARENTALCONTROL=(DIV,ADD,CLEAR,DISPLAY,ICONS,ADVANCE)=>{
 
+    const STYLED=ADVANCE.STYLED;
+
     CLEAR(DIV);
 
     DISPLAY(DIV,`
@@ -14,13 +16,23 @@ const ANDROIDPARENTALCONTROL=(DIV,ADD,CLEAR,DISPLAY,ICONS,ADVANCE)=>{
 
         </div>
 
+        <div class='MessageDiv'>
+
+            <h1 id='Message'></h1>
+
+        </div>
+
         <div class='ChatDivMessages'></div>
 
-        <input class='RequestMovieInput' type='text' placeholder='Enter Your Pin'/>
+        <input class='RequestMovieInput' type='number' placeholder='Enter Your Pin'/>
     
-        <button class='RequestSendButton'>Sumbit</button>
+        <button class='RequestSendButton'></button>
 
     `);
+
+    const MessageDiv=document.querySelector('.MessageDiv');
+        
+    STYLED(MessageDiv,'background',localStorage.getItem('ModeColour'));
 
     
     const BACKICON=document.querySelector('.BackIcon');
@@ -28,6 +40,120 @@ const ANDROIDPARENTALCONTROL=(DIV,ADD,CLEAR,DISPLAY,ICONS,ADVANCE)=>{
     BACKICON.addEventListener('click',()=>{
 
         ANDROIDSETTINGSPAGE(DIV,ADD,CLEAR,DISPLAY,ICONS,ADVANCE);
+
+    })
+
+    const ParentalControlButton=document.querySelector('.RequestSendButton');
+
+    const ParentalControlInput=document.querySelector('.RequestMovieInput');
+
+    const MESSAGE=document.querySelector('#Message')
+
+    if (localStorage.getItem('ParentalControlPin')) {
+                 
+        DISPLAY(ParentalControlButton,`ON`)
+        
+    } else {
+        
+        DISPLAY(ParentalControlButton,`OFF`)
+
+    }
+
+
+    ParentalControlButton.addEventListener('click',()=>{
+
+        if (ParentalControlInput.value.length>=5) {
+
+            const STOREDPIN=localStorage.getItem('ParentalControlPin');
+
+            const SETPIN=ParentalControlInput.value;
+
+            if (STOREDPIN===SETPIN) {
+                
+                localStorage.removeItem('ParentalControlPin');
+
+                ParentalControlButton.innerHTML='OFF';
+
+                setTimeout(() => {
+    
+                    ParentalControlInput.value='';
+
+                    ParentalControlButton.innerHTML='ON'; 
+                    
+                }, 2000);
+
+            } else {
+                
+                
+                STYLED(MessageDiv,'height','50px')
+
+                MESSAGE.innerHTML='Enter Correct Pin';
+
+                setTimeout(() => {
+                    
+                    ParentalControlInput.value='';
+
+                    STYLED(MessageDiv,'height','0')
+
+                    MESSAGE.innerHTML='';
+
+                    if (localStorage.getItem('ParentalControlPin')) {
+                     
+                        ParentalControlButton.innerHTML='ON'; 
+                        
+                    } else {
+                        
+                        ParentalControlButton.innerHTML='OFF'; 
+    
+                    }
+                    
+                }, 2000);
+
+            }
+
+            if(!STOREDPIN){
+
+                localStorage.setItem('ParentalControlPin',SETPIN);
+
+                ParentalControlButton.innerHTML='OFF';
+
+                setTimeout(() => {
+    
+                    ParentalControlInput.value='';
+
+                    ParentalControlButton.innerHTML='ON'; 
+                    
+                }, 2000);
+
+            }
+                  
+        } else {
+
+            STYLED(MessageDiv,'height','50px')
+
+            MESSAGE.innerHTML='Enter Atleast 5 digit Pin';
+            
+            setTimeout(() => {
+
+                ParentalControlInput.value='';
+
+                MESSAGE.innerHTML='';
+
+                STYLED(MessageDiv,'height','0')
+
+                if (localStorage.getItem('ParentalControlPin')) {
+
+                    ParentalControlButton.innerHTML='OFF'; 
+                    
+                } else {
+                    
+                    ParentalControlButton.innerHTML='ON'; 
+
+                }
+                
+            }, 2000);
+
+        }
 
     })
 
--- index.js
+++ index.js
@@ -23,6 +23,21 @@
 
 const DIV=document.querySelector('div');
 
+const BODY=document.querySelector('body');
+
+  // Now you can use the StatusBar API
+  let STATUSMODE = localStorage.getItem('ModeColour');
+
+  if (!STATUSMODE) {
+      // Set default color if nothing is in localStorage
+      STATUSMODE = '#5C829A'; // Replace with your default color
+      localStorage.setItem('ModeColour', STATUSMODE); // Set default color in localStorage
+  }
+
+  // Set the background color of DIV
+  DIV.style.background = STATUSMODE;
+  BODY.style.background = STATUSMODE;
+
 localStorage.setItem('Device','Android');
 
 
