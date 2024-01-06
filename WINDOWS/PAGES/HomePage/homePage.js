f1e35c74db96faa23aca73568d050e95fdfd1762
e-corpcompanygroupmovieslander <movielander1999@gmail.com>
2024-01-06 15:49:56 +0300

    WINDOWS MODES WORKED ON

--- WINDOWS/PAGES/HomePage/homePage.js
+++ WINDOWS/PAGES/HomePage/homePage.js
@@ -1,3 +1,5 @@
+import { STYLED } from "../../../CONNECTION/Connection.js";
+
 const WINDOWSHOMEPAGE=(DIV,ADD,CLEAR,DISPLAY,ICONS,ADVANCE)=>{
 
     CLEAR(DIV);
@@ -6,18 +8,73 @@ const WINDOWSHOMEPAGE=(DIV,ADD,CLEAR,DISPLAY,ICONS,ADVANCE)=>{
     
     <div class='WindowsHeader'>
 
-        <img src='${ICONS}moon.png' class=''/>
+        <div class='WindowsrightHolder'>
 
-        <img src='${ICONS}sun.png' class=''/>
+            <img src='${ICONS}notifications.png' class=''/>
 
-        <img src='${ICONS}user.png' class=''/>
+            <img src='${ICONS}sun.png' class='WindowsLight'/>
+
+            <img src='${ICONS}moon.png' class='WindowsDark'/>
+
+            <img src='${ICONS}user.png' class=''/>
+
+        </div>
+
+ 
 
     
     </div>
 
     <div class='WindowsDivHolder'></div>
 
-    `)
+    `);
+
+    //APPMODES
+
+    const LIGHTMODE=document.querySelector('.WindowsLight');
+
+    const DARKMODE=document.querySelector('.WindowsDark');
+
+    if (localStorage.getItem('ModeColour')==='#5C829A') {
+
+        STYLED(DARKMODE,'display','block');
+
+        STYLED(LIGHTMODE,'display','none');
+        
+   
+
+    } else {
+        
+        STYLED(LIGHTMODE,'display','block');
+
+        STYLED(DARKMODE,'display','none');
+       
+    }
+
+    LIGHTMODE.addEventListener('click',()=>{
+
+        ADVANCE.ADDSTORAGE('local','ModeColour','#212121');
+
+        DIV.style.background='#212121';
+
+        STYLED(DARKMODE,'display','block');
+
+        STYLED(LIGHTMODE,'display','none');
+
+    })
+
+    DARKMODE.addEventListener('click',()=>{
+
+        ADVANCE.ADDSTORAGE('local','ModeColour','#5C829A');
+
+        STYLED(LIGHTMODE,'display','block');
+
+        STYLED(DARKMODE,'display','none');
+
+        DIV.style.background='#5C829A';
+
+    })
+
 
 }
 
