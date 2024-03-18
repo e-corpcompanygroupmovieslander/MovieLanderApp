0e156b044f51f389083edfe73b8335b55acab359
E-corp  Company Group Movie Lander <movielander1999@gmail.com>
2024-03-19 01:19:45 +0300

    APP ENVIRONMENT CORRECTED

--- START/AppStart.js
+++ START/AppStart.js
@@ -1,5 +1,6 @@
 import { VERIFY } from "../CONNECTION/Connection.js"
 import { BODY } from "../RESOURCES/COMPONENTS/BODY/Body.js";
+import { ENVIRONMENT } from "../RESOURCES/FUNCTIONS/ENVIRONMENT/Environment.js";
 
 const CONNECTION=(DIV)=>{
 
@@ -7,7 +8,8 @@ const CONNECTION=(DIV)=>{
 
     VERIFY(DIV);
 
-      
+    ENVIRONMENT('');
+     
 }
 
 export { CONNECTION }
