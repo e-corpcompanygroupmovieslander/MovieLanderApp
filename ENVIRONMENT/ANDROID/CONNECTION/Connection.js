c234e6da80b20f3f429a10faa2831ca650fe595c
E-corp  Company Group Movie Lander <movielander1999@gmail.com>
2024-03-19 07:23:12 +0300

    CLEANED UP ALL MESSED CODE

--- ENVIRONMENT/ANDROID/CONNECTION/Connection.js
+++ ENVIRONMENT/ANDROID/CONNECTION/Connection.js
@@ -1,34 +1,8 @@
-import { BUTTON } from "../../../RESOURCES/COMPONENTS/BUTTON/Button.js"
-import { TEXT } from "../../../RESOURCES/COMPONENTS/TEXT/Text.js"
-import { BREAK } from "../../../RESOURCES/FUNCTIONS/BREAK/Break.js"
-import { CLEAR } from "../../../RESOURCES/FUNCTIONS/CLEAR/Clear.js"
 import { CONDITION } from "../../../RESOURCES/FUNCTIONS/CONDITION/Condition.js"
 import { HOMEPAGE } from "../PAGES/HOMEPAGE/HomePage.js"
 import { LOGINPAGE } from "../PAGES/LOGINPAGE/LoginPage.js"
 
 const ANDROIDCONNECTION=(DIV)=>{
-
-    /*
-
-    CLEAR(DIV);
-
-    BREAK(DIV),BREAK(DIV),BREAK(DIV),BREAK(DIV);
-
-    TEXT(DIV,'h1','App Under Maintence','',()=>{})
-
-    BREAK(DIV),
-    BREAK(DIV),
-    BREAK(DIV),
-    BREAK(DIV);
-
-    BUTTON(DIV,'Contact Us','','',()=>{
-
-        open('https://www.e-corpcompanygroup.com');
-
-    })
-
-
-    */
 
     CONDITION(!localStorage.getItem('User'),
 
